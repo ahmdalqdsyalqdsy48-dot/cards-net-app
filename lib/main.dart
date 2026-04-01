b2dcb64690a56369bd1611ac574501dcbfa4865e
ahmdalqdsyalqdsy48-dot <ahmdalqdsyalqdsy48@gmail.com>
2026-04-02 00:51:06 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart'; // 👈 1. استدعاء مكتبة فايربيس
 
 // استدعاء العقول (Providers) التي أنشأناها
 import 'core/providers/theme_provider.dart';
-import 'core/providers/system_provider.dart'; // 👈 1. استدعاء الخادم المحلي الشامل
+import 'core/providers/system_provider.dart'; 
 
 import 'features/auth/screens/sso_login_screen.dart';
 
-void main() {
+// 👇 2. أضفنا كلمة async لكي ننتظر اتصال فايربيس قبل رسم الشاشات
+void main() async {
+  // 3. هذا السطر ضروري جداً لتهيئة محرك فلاتر قبل الاتصال بالإنترنت
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 4. 👈 كود الاتصال السحري بمشروعك الحقيقي على الإنترنت باستخدام مفاتيحك
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyBV60g3WTr8Kf8wD0bSN2P-8aKn3efqYXk",
+      authDomain: "cards-net-app.firebaseapp.com",
+      projectId: "cards-net-app",
+      storageBucket: "cards-net-app.firebasestorage.app",
+      messagingSenderId: "504008355647",
+      appId: "1:504008355647:web:c236e55cd00d7c5c8d6d8a",
+    ),
+  );
+
   runApp(
     MultiProvider(
       providers: [
         // تفعيل مزود الألوان
         ChangeNotifierProvider(create: (context) => ThemeProvider()),
-        // 👇 2. تفعيل مزود النظام الشامل ليكون متاحاً لجميع الشاشات
+        // تفعيل مزود النظام الشامل ليكون متاحاً لجميع الشاشات
         ChangeNotifierProvider(create: (context) => SystemProvider()), 
       ],
       child: const MyApp(),
