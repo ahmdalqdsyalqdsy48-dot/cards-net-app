f7e4490676ba7611ab17c5fc41396ac07caf054f
ahmdalqdsyalqdsy48-dot <ahmdalqdsyalqdsy48@gmail.com>
2026-05-03 02:31:51 +0300

    Create print_screen.dart

---
+++ lib/features/agent_panel/screens/print_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PrintScreen extends StatelessWidget {
+  const PrintScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('طباعة الكروت')),
+      body: const Center(
+        child: Text('شاشة الطباعة قيد التطوير'),
+      ),
+    );
+  }
+}
