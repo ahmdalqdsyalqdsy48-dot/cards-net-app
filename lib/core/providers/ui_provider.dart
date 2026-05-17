c57661d2fdca033c02919fd23e66c8aee00c2a7a
ahmdalqdsyalqdsy48-dot <ahmdalqdsyalqdsy48@gmail.com>
2026-05-18 00:34:10 +0300

    Update ui_provider.dart

--- lib/core/providers/ui_provider.dart
+++ lib/core/providers/ui_provider.dart
@@ -7,11 +7,15 @@ import 'package:shared_preferences/shared_preferences.dart';
 class UiProvider extends ChangeNotifier {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
-  // 🔊 تجمعات صوتية بدلاً من مشغلات مفردة
-  late final AudioPool _clickPool;
-  late final AudioPool _successPool;
-  late final AudioPool _errorPool;
-  late final AudioPool _notifPool;
+  // 🔊 عدة مشغلات صوتية لكل نوع لتمكين التشغيل المتداخل
+  final List<AudioPlayer> _clickPlayers = [];
+  final List<AudioPlayer> _successPlayers = [];
+  final List<AudioPlayer> _errorPlayers = [];
+  final List<AudioPlayer> _notifPlayers = [];
+  int _clickIndex = 0;
+  int _successIndex = 0;
+  int _errorIndex = 0;
+  int _notifIndex = 0;
 
   bool _isOnline = true;
   List<Map<String, dynamic>> _unreadNotifications = [];
@@ -23,23 +27,27 @@ class UiProvider extends ChangeNotifier {
 
   UiProvider(String? currentUserId) {
     _currentUserPhone = currentUserId;
-    // إعداد المجمعات الصوتية (تدعم عدة مشغلات متزامنة)
-    _clickPool = AudioPool(
-      source: AssetSource('sounds/click.mp3'),
-      maxPlayers: 3,   // يسمح بـ 3 نقرات متداخلة
-    );
-    _successPool = AudioPool(
-      source: AssetSource('sounds/success.mp3'),
-      maxPlayers: 2,
-    );
-    _errorPool = AudioPool(
-      source: AssetSource('sounds/error.mp3'),
-      maxPlayers: 2,
-    );
-    _notifPool = AudioPool(
-      source: AssetSource('sounds/notification.mp3'),
-      maxPlayers: 2,
-    );
+    // إعداد عدة مشغلات لكل نوع (3 للـ click، 2 للبقية)
+    for (int i = 0; i < 3; i++) {
+      final player = AudioPlayer();
+      player.setSource(AssetSource('sounds/click.mp3'));
+      _clickPlayers.add(player);
+    }
+    for (int i = 0; i < 2; i++) {
+      final player = AudioPlayer();
+      player.setSource(AssetSource('sounds/success.mp3'));
+      _successPlayers.add(player);
+    }
+    for (int i = 0; i < 2; i++) {
+      final player = AudioPlayer();
+      player.setSource(AssetSource('sounds/error.mp3'));
+      _errorPlayers.add(player);
+    }
+    for (int i = 0; i < 2; i++) {
+      final player = AudioPlayer();
+      player.setSource(AssetSource('sounds/notification.mp3'));
+      _notifPlayers.add(player);
+    }
 
     _monitorInternetConnection();
     if (currentUserId != null) {
@@ -71,22 +79,34 @@ class UiProvider extends ChangeNotifier {
     if (value) playSound('success');
   }
 
-  /// 🔊 تشغيل الأصوات (مع احترام كتم الصوت)
+  /// 🔊 تشغيل الأصوات (مع احترام كتم الصوت والسماح بالتداخل)
   Future<void> playSound(String type) async {
     if (!_soundsEnabled) return;
     try {
       switch (type) {
         case 'click':
-          await _clickPool.start(volume: 0.5);
+          final player = _clickPlayers[_clickIndex % _clickPlayers.length];
+          _clickIndex++;
+          await player.stop(); // إيقاف السابق إن وجد
+          await player.play(AssetSource('sounds/click.mp3'), volume: 0.5);
           break;
         case 'success':
-          await _successPool.start(volume: 1.0);
+          final player = _successPlayers[_successIndex % _successPlayers.length];
+          _successIndex++;
+          await player.stop();
+          await player.play(AssetSource('sounds/success.mp3'), volume: 1.0);
           break;
         case 'error':
-          await _errorPool.start(volume: 0.8);
+          final player = _errorPlayers[_errorIndex % _errorPlayers.length];
+          _errorIndex++;
+          await player.stop();
+          await player.play(AssetSource('sounds/error.mp3'), volume: 0.8);
           break;
         case 'notification':
-          await _notifPool.start(volume: 1.0);
+          final player = _notifPlayers[_notifIndex % _notifPlayers.length];
+          _notifIndex++;
+          await player.stop();
+          await player.play(AssetSource('sounds/notification.mp3'), volume: 1.0);
           break;
       }
     } catch (e) {
