80a8d732e51964a4782f94d8a201a111e3092ef9
ahmdalqdsyalqdsy48-dot <ahmdalqdsyalqdsy48@gmail.com>
2026-05-26 23:02:44 +0300

    Update sound_service.dart

--- lib/core/services/sound_service.dart
+++ lib/core/services/sound_service.dart
@@ -1,116 +1,80 @@
 import 'package:audioplayers/audioplayers.dart';
-import 'package:flutter/foundation.dart';
 import '../providers/settings_provider.dart';
 
 class SoundService {
   final SettingsProvider _settings;
 
-  // عدة مشغلات لكل نوع (مثل النظام القديم الناجح)
-  final List<AudioPlayer> _clickPlayers = [];
-  final List<AudioPlayer> _successPlayers = [];
-  final List<AudioPlayer> _errorPlayers = [];
-  final List<AudioPlayer> _warningPlayers = [];
-  final List<AudioPlayer> _tapPlayers = [];
-  final List<AudioPlayer> _notifPlayers = [];
-
-  int _clickIdx = 0, _succIdx = 0, _errIdx = 0, _warnIdx = 0, _tapIdx = 0, _notifIdx = 0;
+  late final AudioPool _clickPool;
+  late final AudioPool _successPool;
+  late final AudioPool _errorPool;
+  late final AudioPool _warningPool;
+  late final AudioPool _tapPool;
+  late final AudioPool _notifPool;
 
   SoundService(this._settings) {
-    _initPlayers();
-  }
-
-  void _initPlayers() {
-    // 3 مشغلات للنقر، 2 للبقية
-    for (int i = 0; i < 3; i++) {
-      _clickPlayers.add(AudioPlayer());
-    }
-    for (int i = 0; i < 2; i++) {
-      _successPlayers.add(AudioPlayer());
-      _errorPlayers.add(AudioPlayer());
-      _warningPlayers.add(AudioPlayer());
-      _tapPlayers.add(AudioPlayer());
-      _notifPlayers.add(AudioPlayer());
-    }
-    _preloadAll();
-  }
-
-  Future<void> _preloadAll() async {
-    final sources = {
-      _clickPlayers: 'sounds/click.mp3',
-      _successPlayers: 'sounds/success.mp3',
-      _errorPlayers: 'sounds/error.mp3',
-      _warningPlayers: 'sounds/warning.mp3',
-      _tapPlayers: 'sounds/tap.mp3',
-      _notifPlayers: 'sounds/notification.mp3',
-    };
-    for (final entry in sources.entries) {
-      for (final player in entry.key) {
-        await player.setSource(AssetSource(entry.value));
-      }
-    }
+    _clickPool = AudioPool(
+      AssetSource('sounds/click.mp3'),
+      maxPlayers: 3,
+    );
+    _successPool = AudioPool(
+      AssetSource('sounds/success.mp3'),
+      maxPlayers: 2,
+    );
+    _errorPool = AudioPool(
+      AssetSource('sounds/error.mp3'),
+      maxPlayers: 2,
+    );
+    _warningPool = AudioPool(
+      AssetSource('sounds/warning.mp3'),
+      maxPlayers: 2,
+    );
+    _tapPool = AudioPool(
+      AssetSource('sounds/tap.mp3'),
+      maxPlayers: 2,
+    );
+    _notifPool = AudioPool(
+      AssetSource('sounds/notification.mp3'),
+      maxPlayers: 2,
+    );
   }
 
   Future<void> play(String type) async {
     if (!_settings.isSoundEnabled) return;
 
-    List<AudioPlayer>? players;
-    int? index;
-    double volume;
-    String? assetPath;
-
-    switch (type) {
-      case 'click':
-        players = _clickPlayers;
-        index = _clickIdx++ % players.length;
-        volume = 0.5;
-        assetPath = 'sounds/click.mp3';
-        break;
-      case 'success':
-        players = _successPlayers;
-        index = _succIdx++ % players.length;
-        volume = 1.0;
-        assetPath = 'sounds/success.mp3';
-        break;
-      case 'error':
-        players = _errorPlayers;
-        index = _errIdx++ % players.length;
-        volume = 0.8;
-        assetPath = 'sounds/error.mp3';
-        break;
-      case 'warning':
-        players = _warningPlayers;
-        index = _warnIdx++ % players.length;
-        volume = 0.9;
-        assetPath = 'sounds/warning.mp3';
-        break;
-      case 'tap':
-        players = _tapPlayers;
-        index = _tapIdx++ % players.length;
-        volume = 0.3;
-        assetPath = 'sounds/tap.mp3';
-        break;
-      case 'notification':
-        players = _notifPlayers;
-        index = _notifIdx++ % players.length;
-        volume = 1.0;
-        assetPath = 'sounds/notification.mp3';
-        break;
-      default:
-        return;
-    }
-
-    final player = players.elementAt(index);
     try {
-      await player.stop();
-      await player.play(AssetSource(assetPath!), volume: volume);
+      switch (type) {
+        case 'click':
+          await _clickPool.play(volume: 0.5);
+          break;
+        case 'success':
+          await _successPool.play(volume: 1.0);
+          break;
+        case 'error':
+          await _errorPool.play(volume: 0.8);
+          break;
+        case 'warning':
+          await _warningPool.play(volume: 0.9);
+          break;
+        case 'tap':
+          await _tapPool.play(volume: 0.3);
+          break;
+        case 'notification':
+          await _notifPool.play(volume: 1.0);
+          break;
+        default:
+          break;
+      }
     } catch (e) {
       debugPrint('SoundService error ($type): $e');
     }
   }
 
   void dispose() {
-    for (final list in [_clickPlayers, _successPlayers, _errorPlayers, _warningPlayers, _tapPlayers, _notifPlayers]) {
-      for (final p in list) p.dispose();
-    }
+    _clickPool.dispose();
+    _successPool.dispose();
+    _errorPool.dispose();
+    _warningPool.dispose();
+    _tapPool.dispose();
+    _notifPool.dispose();
   }
 }
