9a958ff9c9dc8a195ac6fbfdf2cab89b26d5a1b3
ahmdalqdsyalqdsy48-dot <ahmdalqdsyalqdsy48-dot@users.noreply.github.com>
2026-06-10 23:47:53 +0000

    deploy: 9d0897fceb067542781e0a6d2215696f7a06bfeb

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1485331429"
+    serviceWorkerVersion: "3652921493"
   }
 });
