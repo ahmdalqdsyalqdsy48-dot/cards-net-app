6bd25090d113f37b112651324a9c3793d6bb3b4a
ahmdalqdsyalqdsy48-dot <ahmdalqdsyalqdsy48-dot@users.noreply.github.com>
2026-08-08 22:59:28 +0000

    deploy: 1c30583858719f601c1ea6e50af62e97f16ee91a

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "264c8a04bded31725f252c8be9b7fc12",
+const RESOURCES = {"flutter_bootstrap.js": "476ea834fb7487906540285997c7e57e",
 "index.html": "349abaa9bfd2c74098e2ee0ea351f841",
 "/": "349abaa9bfd2c74098e2ee0ea351f841",
 "main.dart.js": "91ea5aa1fd52bd5d5b15fc8cc535f1a6",
